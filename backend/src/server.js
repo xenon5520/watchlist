36f9eac29a3a3239a0884d0b92fc7f6a2ddc5dc4
xenon5520 <chmcs2026.aditya.jha@gmail.com>
2026-03-04 12:26:17 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3001
 app.use(cors(
     {
-        origin: 'http://localhost:5173'
+        //origin: 'http://localhost:5173'
     }
 ))
 app.use(express.json())
